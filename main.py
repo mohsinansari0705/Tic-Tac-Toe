c6f2be610bb923b090eacd4e80879d87184d6d04
Mohsin Ansari <mohsinansari0705@gmail.com>
2024-11-20 19:46:03 +0530

    Complete game features added

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-import game_functions as func
-from settings import filled_box
+import pygame as pg
+import game_functions as g_func
+import settings as sett
 
+pg.init()
 
-if __name__=="__main__":
-    player = "O"
-    print("_____ Play Tic-Tac-Toe _____\n")
-    func.print_board()
-    while True:
-        if(filled_box == 9):
-            print(func.check_winner())
-            break
-        else:
-            if(func.check_winner()):
-                print(func.check_winner())
-                break
-            else:
-                filled_box+=1
+pg.display.set_icon(pg.image.load('./assets/images/logo.png'))
+pg.display.set_caption("Tic Tac Toe :Falcon")
 
-                if(player == "O"):
-                    func.player_turn(player)
-                    player = "X"
-                else:
-                    func.player_turn(player)
-                    player = "O"
+running = True
+while running:
+    sett.screen.fill('#1f1f1f')
+    g_func.play_game()
+
+    for event in pg.event.get():
+        if(event.type == pg.QUIT):
+            running = False
+
+        if(event.type == pg.KEYDOWN):
+            if(event.key == pg.K_SPACE):
+                sett.game_status = True
+
+            if(event.key == pg.K_ESCAPE):
+                sett.game_status = False
+                sett.display_winner_status = False
+
+            if(sett.game_status==True and sett.display_winner_status==True):
+                if(event.key == pg.K_SPACE):
+                    g_func.reset_game()
+
+        if(sett.game_status == True):
+            if(event.type == pg.MOUSEBUTTONDOWN):
+                mouse_x, mouse_y = pg.mouse.get_pos()
+                g_func.place_value(mouse_x-59, mouse_y-125)
+
+    pg.display.update()
